59bf9edbd93e39db531d8cbea38bcefe9711f4f9
Dmitry Makovskiy <64654245+DMakovskiy@users.noreply.github.com>
2024-05-10 21:57:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+import csv
+from datetime import datetime
+
+
+def find_by_key(iterable, key, value):
+    # находит в списке словарей словарь с указанным ключом и значением и возвращает индекс этого словаря и сам словарь.
+    # если такого словаря в списке нет, возвращает кортеж (-1, -1)
+    for index, dict_ in enumerate(iterable):
+        if key in dict_ and dict_[key] == value:
+            return index, dict_
+    return -1, -1
+
+
+with open('exam_results.csv', 'r', encoding='utf-8') as inp_file:
+    students = csv.DictReader(inp_file)  # считываем файл
+    best_scores = [] # финальный список словарей
+    pattern = '%Y-%m-%d %H:%M:%S'  # шаблон строки для преобразования в datetime
+    for stud in students:
+        stud['best_score'] = int(stud.pop('score')) # меняем название ключа и делаем значение ключа целочисленным
+        # проверяем, есть ли уже в финальном списке словарь с таким же email
+        i, d = find_by_key(best_scores, 'email', stud['email'])
+        if i != -1:
+        # если словарь с таким email уже имеется, сравниваем оценки; если оценки равны, сравниваем даты
+            if int(stud['best_score']) > (int(d['best_score'])) or \
+                    (stud['best_score'] == d['best_score'] and
+                     datetime.strptime(stud['date_and_time'], pattern) > datetime.strptime(d['date_and_time'], pattern)):
+                best_scores.pop(i)
+                best_scores.append(stud)
+        else:
+            best_scores.append(stud)
+    best_scores.sort(key=lambda x: x['email'])
+
+with open('best_scores.json', 'w') as out_file:
+    json.dump(best_scores, out_file, indent=3)
